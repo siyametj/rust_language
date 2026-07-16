08b94c70d47a06d2768c270a4f81b270c32fda18
usep-arch <s93753564@gmail.com>
2026-07-16 17:22:21 +0600

    Hell world in rust language

---
+++ src/hello_world.rs
@@ -0,0 +1,19 @@
+// hello_world.rs
+
+pub fn hello_world() {
+    // Print hello world in the terminal
+    println!("Hello world");
+    println!("Hi! It's siyam.");
+
+    // What is Macro?
+    println!(); // Print just a new line
+    println!("Hello! It's miss Aria"); // Print text
+    println!("My name is {}", "siyam"); // Prints format some arguments
+
+    // Comments in Rust. How look this?
+    // This is single line comment. Here use // (Double Forward Slash)
+    /*
+    This is a
+    Multi-Line comment
+    */
+}
