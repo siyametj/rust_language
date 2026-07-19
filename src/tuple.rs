402e94ce0a87a5fda87d27a803930b5d4ef72d44
usep-arch <s93753564@gmail.com>
2026-07-19 19:33:39 +0600

    tuple in rust language

---
+++ src/tuple.rs
@@ -0,0 +1,11 @@
+// tuple.rs
+
+pub fn tuple() {
+    // Syntax 1
+    // let tuple_name: (data_type_1, data_type_2, data_type_3) = (value_1, value_2, value_3);
+
+    // Syntax 2
+    // let tuple_name = (value_1, value_2, value_3);
+
+
+}
