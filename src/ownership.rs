3270f3b354ee5344b8eb5ce2e639517001ee7acd
usep-arch <s93753564@gmail.com>
2026-07-21 11:04:24 +0600

    feat: Ownership in rust language

--- src/ownership.rs
+++ src/ownership.rs
@@ -14,5 +14,34 @@ pub fn ownership() {
     // Clone
     let name1: String = String::from("Miss Aria");
     let name2: String = name1.clone();
-    println!("Name 1: {name1}\nName 2: {name2}");
+    println!("Name 1: {name1}\nName 2: {name2}\n");
+
+    // Function and ownership
+    fn take_string(string: String) {
+        println!("The input is about: {string}");
+    } // string finish here
+
+    let my_string: String = String::from("I like pizza!");
+    // take_string function take ownership of my_string
+    take_string(my_string);
+    // println!("My string: {my_string}"); // its not alive
+    println!();
+
+    // Function and return
+    fn give_ownership() -> String {
+        let new_string: String = String::from("Aria love pizza!");
+        new_string
+    }
+    let new_string: String = give_ownership();
+    println!("New string: {new_string}\n");
+
+    // Function with clone
+    // give clone! Not main
+    let my_string: String = String::from("Aria don't like burger!");
+    my_func(my_string.clone());
+    println!("My string: {my_string}"); // it works
 }
+
+fn my_func(string: String) {
+    println!("New string: {string}");
+} // string is finish here
