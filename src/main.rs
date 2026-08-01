d4ac2ea6b801c37da2f1a80a6ef51d3cc282f11f
usep-arch <s93753564@gmail.com>
2026-08-01 12:01:41 +0600

    ---

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,37 @@
 // mod data_types;
 // mod type_casting;
 // mod variable;
-mod constants;
+// mod constants;
+// mod string;
+// mod operators;
+// mod decision_making;
+// mod loops;
+// mod function;
+// mod tuple;
+// mod ownership;
+// mod borrowing_and_references;
+// mod scope;
+// mod vectors;
+// mod rust_structs;
+mod rust_impl;
+
 
 fn main() {
     // hello_world::hello_world();
     // data_types::data_types();
     // type_casting::type_casting();
     // variable::variable();
-    constants::constants();
-
+    // constants::constants();
+    // string::string();
+    // operators::operators();
+    // decision_making::decision_making();
+    // loops::loops();
+    // function::function();
+    // tuple::tuple();
+    // ownership::ownership();
+    // borrowing_and_references::borrowing_and_references();
+    // scope::scope();
+    // vectors::vectros();
+    // rust_structs::rust_structs();
+    rust_impl::rust_impl();
 }
